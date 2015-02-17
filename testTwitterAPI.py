69a842cb7499a050b848b18978dc685057ada551
Xiaochen <weixc123@gmail.com>
2015-02-16 22:33:04 -0500

    test twitter API

---
+++ testTwitterAPI.py
@@ -0,0 +1,20 @@
+__author__ = 'wei'
+import tweepy
+
+consumer_key = "anbpwuMUw7nIlo5SXAxCU803j"
+
+consumer_secret = "u2yVCFbi7os8dFrSUwmk4zzGRuyLMWkghHw1HcSiIMvSqHPp2p"
+
+access_token = "761246203-hy3aSbOM1ct3dKc0MNLU15hUL3aeHSJrLSHKPBEK"
+access_token_secret = "aqiOnSL0MG2KxSdcYuASJY8HuZq9svjLb4X97KSSFmvs2"
+
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+
+api = tweepy.API(auth)
+
+public_tweets = api.search("columbia")
+for tweet in public_tweets:
+    if tweet.geo is not None:
+        print tweet.text
+        print tweet.geo
