c2fc883a886592f9c36fc249c5a09acf3a0b4325
Xiaochen <weixc123@gmail.com>
2015-03-02 19:29:08 -0500

    update

--- testDB.py
+++ testDB.py
@@ -1,6 +1,6 @@
 __author__ = 'wei'
 import MySQLdb as sql
 import MySQLdb.cursors
-
 db = sql.connect(host='cloud.comtnuycjpkv.us-west-2.rds.amazonaws.com', user='weixc1234', passwd='wxc16888', db='innodb', cursorclass=MySQLdb.cursors.DictCursor)
 cursor = db.cursor()
+
---
+++ website/forms.py
@@ -0,0 +1,5 @@
+from WTForms import Form, TextField, BooleanField
+
+ 
+class LoginForm(Form):
+    pass
